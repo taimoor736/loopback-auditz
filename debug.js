43529802788c630c20a1c09d21daeb5f4618dc66
taimoorali736 <taimur.ali@blueeast.com>
2017-11-17 14:50:11 +0500

    Aded the debug file

---
+++ debug.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _debug = require('debug');
+
+var _debug2 = _interopRequireDefault(_debug);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'auditz';
+  return (0, _debug2.default)('loopback:mixins:' + name);
+};
+
+module.exports = exports['default'];
+//# sourceMappingURL=debug.js.map
